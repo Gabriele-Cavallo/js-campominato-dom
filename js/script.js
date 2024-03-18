e6e25b33592cd80a0a2f2b10983873082635bff5
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-18 12:54:34 +0100

    feat: add gridBombs array + gridBombsRandom function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,21 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 // ed emetto un messaggio in console con il numero della cella cliccata.
 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. 
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe 
+// non potranno esserci due numeri uguali.
+
+
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo 
+// calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora 
+// di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di 
+// numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha 
+// cliccato su una cella che non era una bomba.
+
 // Bonus
 
 // Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
@@ -13,6 +28,9 @@
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
+
+// Dichiaro l'array che andrà a conteneri i numeri corrispondenti alle celle delle bombe
+let gridBombs = [];
 // - Genero l'elemento del dom al cui click si crea la tabella di gioco
 let play = document.querySelector('#play');
 // - Creo l'evento click che fa apparire la tabella
@@ -63,8 +81,6 @@ function clickSquare(gridItem){
     });
 }
 
-
-
 // funzione per attivare o disattivare la griglia di gioco al click del bottone play
 function playGrid(){
     play.addEventListener('click', function(){
@@ -72,12 +88,16 @@ function playGrid(){
         difficulty();
         // - Attivo o nascondo la griglia in base al suo stato attuale
         if(active === true){
+            // Svuoto l'array precedentemente creato
+            gridBombs = [];
             userGrid.classList.remove('active');
             userGrid.classList.add('hide');  
             userGrid.innerHTML = '';
             play.innerHTML = 'Open';
             active = false; 
         }else if(active === false){
+            // Popolo l'array con i 16 numeri random che coincidono con le caselle delle bombe
+            gridBombsRandom();
             // - Genero una griglia in base alla difficoltà impostata
             for (let i = 0; i < difficoltà; i++) {
                 let gridSquare = createSquare(i + 1);
@@ -95,4 +115,16 @@ function playGrid(){
     });
     
 }
+
+// funzione che genera 16 numeri random per indicare quali celle sono le bombe e le pusha nell'array
+function gridBombsRandom(){
+    while (gridBombs.length < 16) {
+        let bombs = Math.floor((Math.random() * 100) + 1);
+        if (!gridBombs.includes(bombs)){
+            gridBombs.push(bombs);
+        }
+    }
+}
+
+
 
